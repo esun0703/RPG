4db53c4c03d316d75bd50450c194790601d878c7
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2016-12-06 17:16:44 -0500

    Added index.html, images, style.css, bootstrap.css and added variables to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+//Variables
+//===============================================================================
+//Characters
+var charName = ["Natsu Dragneel", "Erza Scarlett", "Lucy Heartfilia", "Gray Fullbuster" ];
+var charHealth = [3000, 3200, 2900, 3100];
+var charPic =['../images/natsu.png','../images/erza.png', '../images/lucy.png','../images/gray.png'];
+var charHit=[]
+
+//user and opponent variables
+var userHealth;
+var userAttack;
+var opponentHealth;
+var opponentAttack;
+
+
+
+
+
+
+
+
+
+
+
+
+//Functions
+//===============================================================================
+$(".close").on("click", function(){
+	$('.alert').remove();
+});
+
+
+
+
+//Gameplay
+//===============================================================================
